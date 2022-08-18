e94928662aa18859409697eb97b03dc4b64d7095
Lucas Trindade <trslucas@outlook.com>
2022-08-18 09:43:30 -0300

    Formulários: Typescript no Formulário

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,17 +15,23 @@ import {
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, "Informe a tarefa"),
-
   minutesAmount: zod.number().min(5).max(60),
 });
 
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm({
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: "",
+      minutesAmount: 0,
+    },
   });
 
-  function handleCreateNewCycle(data: any) {
+  function handleCreateNewCycle(data: NewCycleFormData) {
     console.log(data);
+    reset();
   }
 
   const task = watch("task");

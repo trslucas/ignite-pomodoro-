195ef6e2d7c9702b5b3d18d16daa80dcd13bdb14
Lucas Trindade <trslucas@outlook.com>
2022-08-19 14:49:17 -0300

    Funcionalidades da aplicação: Diminuindo o countdown

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,8 @@ import { Play } from "phosphor-react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as zod from "zod";
+import { useEffect, useState } from "react";
+import { differenceInSeconds } from "date-fns";
 
 import {
   CountdownContainer,
@@ -12,7 +14,6 @@ import {
   StartCountdownButton,
   TaskInput,
 } from "./styles";
-import { useState } from "react";
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, "Informe a tarefa"),
@@ -25,6 +26,7 @@ interface Cycle {
   id: string;
   task: string;
   minutesAmout: number;
+  startDate: Date;
 }
 
 export function Home() {
@@ -40,12 +42,25 @@ export function Home() {
     },
   });
 
+  const activeCycle = cycles.find((cycle) => cycle.id == activeCycleId);
+
+  useEffect(() => {
+    if (activeCycle) {
+      setInterval(() => {
+        setAmountSecondsPassed(
+          differenceInSeconds(new Date(), activeCycle.startDate)
+        );
+      }, 1000);
+    }
+  }, [activeCycle]);
+
   function handleCreateNewCycle(data: NewCycleFormData) {
     const id = String(new Date().getTime());
     const newCycle: Cycle = {
       id,
       task: data.task,
       minutesAmout: data.minutesAmount,
+      startDate: new Date(),
     };
 
     setCycles((state) => [...state, newCycle]);
@@ -53,9 +68,8 @@ export function Home() {
     reset();
   }
 
-  const activeCycle = cycles.find((cycle) => cycle.id == activeCycleId);
-
   const totalSeconds = activeCycle ? activeCycle.minutesAmout * 60 : 0;
+
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
 
   const minutesAmount = Math.floor(currentSeconds / 60);
